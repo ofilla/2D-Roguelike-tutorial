77aa4db5d2da04e958239f7561ebbf89eb96ff6f
Oliver Filla <ocl@hotmail.de>
2021-05-09 17:24:16 +0200

    Add GameManager and Loader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+
     public BoardManager boardScript;
     public int firstLevel = 3; // start with 3 because first enemy appears here
 
@@ -12,6 +14,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+
         level = firstLevel;
         boardScript = GetComponent<BoardManager>();
         InitGame();
@@ -20,11 +30,5 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
