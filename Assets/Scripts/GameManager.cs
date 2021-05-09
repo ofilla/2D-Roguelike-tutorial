a172f59766d52f4758785e4bbc8c865d43587fd1
Oliver Filla <ocl@hotmail.de>
2021-05-09 17:19:35 +0200

    First version GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public BoardManager boardScript;
+    public int firstLevel = 3; // start with 3 because first enemy appears here
+
+    private int level;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        level = firstLevel;
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
