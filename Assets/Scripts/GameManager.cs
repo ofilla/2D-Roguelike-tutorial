101e7421f5ddb765dae1cc39137d049192a7a661
Oliver Filla <ocl@hotmail.de>
2021-05-13 16:31:44 +0200

    Add Player script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 
     public BoardManager boardScript;
     public int firstLevel = 3; // start with 3 because first enemy appears here
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true; // TODO is this an annotation?
 
     private int level;
 
@@ -28,5 +30,9 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver() {
+        enabled = false;
     }
 }
